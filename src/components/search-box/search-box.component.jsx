4168b8bf1c235fbcf50a1c607cfd05ec58bbe856
Nhu Mong Tran <nhu.mong.tran@fokus.fraunhofer.de>
2020-01-19 15:18:54 +0100

    adding files for github pages

---
+++ src/components/search-box/search-box.component.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import './search-box.styles.css';
+
+//functional component
+export const SearchBox = ({ placeholder, handleChange }) => (
+    <input class="search" type="search" placeholder={placeholder} onChange={handleChange}></input>
+)
