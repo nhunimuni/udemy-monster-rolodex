20b51feec8c54476210d1bf6f19b964119317c14
Nhu Mong Tran <nhu.mong.tran@fokus.fraunhofer.de>
2020-01-19 15:11:28 +0100

    Updates

---
+++ precache-manifest.d662433cb6a0ead3edad39dbfa97dfe9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c10996c1aab97c499683eaddce0fd716",
+    "url": "/udemy-monster-rolodex/index.html"
+  },
+  {
+    "revision": "7f220befed1e17a7f5ed",
+    "url": "/udemy-monster-rolodex/static/css/main.e429127c.chunk.css"
+  },
+  {
+    "revision": "4205f318e26dcb05f19d",
+    "url": "/udemy-monster-rolodex/static/js/2.4d19465e.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/udemy-monster-rolodex/static/js/2.4d19465e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7f220befed1e17a7f5ed",
+    "url": "/udemy-monster-rolodex/static/js/main.4507fe7e.chunk.js"
+  },
+  {
+    "revision": "137da2c1fac62bf28915",
+    "url": "/udemy-monster-rolodex/static/js/runtime-main.b3d4918f.js"
+  }
+]);
